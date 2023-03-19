abba6719b9524f65bb65f9abcb01f2574cf8b2e2
User <gold.like.simple@gmail.com>
2023-03-19 20:59:39 +1000

    [+] OrderPath

---
+++ OrderKeeper/OrderKeeper.Model/Order/OrderPatch.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderKeeper.Model.Order;
+
+public class OrderPatch
+{
+    [Required]
+    public readonly int ProviderId;
+
+    [Required]
+    public readonly string Number;
+
+    public OrderPatch(int providerId, string number)
+    {
+        ProviderId = providerId;
+        Number = number;
+    }
+
+}
