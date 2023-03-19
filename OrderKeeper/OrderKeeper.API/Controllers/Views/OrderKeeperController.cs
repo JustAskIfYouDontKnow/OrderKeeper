9bed2f93ae964b614d03b491198a29f4f8b8b416
User <gold.like.simple@gmail.com>
2023-03-19 23:05:28 +1000

    [+] Add base view routes

---
+++ OrderKeeper/OrderKeeper.API/Controllers/Views/OrderKeeperController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrderKeeper.API.Controllers.Views;
+
+public class OrderKeeperController : Controller
+{
+    
+    public IActionResult Index()
+    {
+        return View("~/Views/Home/Index.cshtml");
+    }
+    
+    public IActionResult CreateOrder()
+    {
+        return View("~/Views/CreateOrder/Index.cshtml");
+    }
+    
+    public IActionResult EditOrder(int orderId)
+    {
+        ViewData["OrderId"] = orderId;
+        return View("~/Views/EditOrder/Index.cshtml");
+    }
+
+}
