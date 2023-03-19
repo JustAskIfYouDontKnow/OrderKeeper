0d40371146c70616797d8879ba8662e5db8753f9
User <gold.like.simple@gmail.com>
2023-03-19 21:03:14 +1000

    [+] Add UpdateOrder and GetOneOrder response form

---
+++ OrderKeeper/OrderKeeper.Client/Order/GetOneOrder.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderKeeper.Client.Order;
+
+public class GetOneOrder
+{
+    public class Response
+    {
+        [Required]
+        public Payload.Order.Order Order { get; set; }
+    }
+}
---
+++ OrderKeeper/OrderKeeper.Client/Order/UpdateOrder.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using OrderKeeper.Model.OrderItem;
+
+namespace OrderKeeper.Client.Order;
+
+public class UpdateOrder
+{
+    [Required]
+    public int OrderId { get; set; }
+
+    [Required]
+    public int ProviderId { get; set; }
+
+    [Required]
+    public string Number { get; set; }
+    
+    [Required]
+    public List<OrderItemPatch> OrderItems { get; set; }
+}
